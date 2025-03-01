5744b727df4aba76033ee6dcc7b6f1f1dd208cf2
NikshitSaini <nikshithansi@gmail.com>
2025-03-01 12:30:50 +0530

    Changes

--- app.py
+++ app.py
@@ -159,4 +159,5 @@ def save_student():
         return "Error saving student", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
